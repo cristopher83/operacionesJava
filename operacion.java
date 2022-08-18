93b5e5082b020088becc315da23cf611c78caa15
cristopher83 <cristopher34234@gmial.com>
2022-08-18 14:29:05 -0500

    First commit

---
+++ operacion.java
@@ -0,0 +1,9 @@
+public class operacion {
+    public static void main(String []args) {
+        int no1 = 5,no2 = 0, SUMA = 0,RESTA=0,MULTIPLICACION=0,DIVISION=0;
+    System.out.println("Operaciones Basicas");
+    SUMA=no1+no2;
+
+    System.out.println(SUMA);
+    }
+}
